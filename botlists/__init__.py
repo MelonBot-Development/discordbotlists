700082365422f3572b0d3bc5e2c6eff5c76839a2
Lemon Rose <japandotorg@users.noreply.github.com>
2022-06-28 22:07:51 +0530

    init done

---
+++ botlists/__init__.py
@@ -0,0 +1,17 @@
+__title__ = "discordbotlists"
+__author__ = "japandotorg"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022, japandotorg"
+__version__ = "1.0.0"
+
+name = "discordbotlists"
+
+from collections import namedtuple
+
+from .base import Base
+from .client import Client
+from .exceptions import *
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel="alpha", serial=0)
